153cee4f79823e4220791c412c994cea1f3904e1
Veronica Nixon <vsnixon@gmail.com>
2020-12-20 19:01:18 -0700

    Tests paths to js and json files.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,18 @@
 
 // Read in json asynchronously with .then.
-d3.json("data/samples.json", function(data)
-{
-    console.log(data);
-});
+//d3.json("data/samples.json", function(data)
+//{
+//    console.log(data);
+//});
+
+console.log("sdkjfhksdjhfk");
+
+// Read in json asynchronously with .then.
+sam_dat_promise = d3.json("data/samples.json");
+function sam_dat_fail() {
+    console.log("failed to get sample data json");
+}
+function sam_dat_worked(val)  {
+    console.log(val);
+};
+sam_dat_promise.then(sam_dat_worked, sam_dat_fail);
