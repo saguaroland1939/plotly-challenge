bd85baae201ef1aad913dc5f2a1e4c8efeb3851c
Veronica Nixon <vsnixon@gmail.com>
2020-12-29 17:27:08 -0700

    Implements static horizontal bar chart.

--- static/js/app.js
+++ static/js/app.js
@@ -12,9 +12,8 @@
 // Import JavaScript object from data.js
 var results = data;
 
-// Read in first name from names array
+// Read in first name from names array. Will be used in drop-down menu.
 var nameDefault = results.names[0];
-console.log(nameDefault);
 
 // Select element in index.html where metadata will be appended
 var metadataElement = d3.select("ul");
@@ -26,6 +25,58 @@ metadataDefault.forEach(function([key, value])
     var metadataItem = metadataElement.append("li");
     metadataItem.text(`${key}: ${value}`);
 });
+
+// Extract otu_ids and convert to formatted string to be used as categorical y-values
+var otu_idsDefault = results.samples[0].otu_ids;
+otu_idsDefaultText = otu_idsDefault.map(x => `OTU-${x}`);
+
+// Extract sample_values to be used as x-values
+var measurementsDefault = results.samples[0].sample_values;
+
+// Extract otu_labels to be used as hovertext
+var hoverDefault = results.samples[0].otu_labels;
+
+// Combine otu_idsDefaultText, measurementsDefault, and hoverDefault arrays into an array of objects so
+// they can be sorted simultaneously
+var combinedLists = [];
+for (var i = 0; i < otu_idsDefaultText.length; i++)
+{
+    combinedLists.push({"otu_idsDefaultText": otu_idsDefaultText[i], "measurementsDefault": measurementsDefault[i], "hoverDefault": hoverDefault[i]});
+}
+
+// Sort the array of objects descending by measurementsDefault
+var combinedListsSorted = combinedLists.sort((a, b) => b.measurementsDefault - a.measurementsDefault);
+
+// Extract top 10 from sorted list
+var top10 = combinedListsSorted.slice(0, 10);
+
+// Separate three arrays for plotting
+var xValues = top10.map(x => x.measurementsDefault);
+var yValues = top10.map(x => x.otu_idsDefaultText);
+var hoverValues = top10.map(x => x.hoverDefault);
+
+// Plot otu_ids and sample_values as horizontal bar chart
+var trace = 
+{
+    x: xValues,
+    y: yValues,
+    type: "bar",
+    orientation: "h",
+    text: hoverValues
+};
+
+var traces = [trace];
+
+var layout = 
+{
+    title: `OTU counts for sample ${nameDefault}`,
+    xaxis: {title: "Count"},
+    yaxis: {title: "OTU ID"}
+};
+
+Plotly.newPlot("bar-chart", traces, layout)
+
+
 // Function to extract data from results based on user's input
 // function switchDataset(userChoice)
 // {
